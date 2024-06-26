6e6c5ff68640ce7447493846a46a71b3654424d9
FredericoGSilva <frederico.dev@outlook.com>
2024-06-25 21:23:31 -0300

    Add UserEntity entity with attributes and mappings

---
+++ cadastro/src/main/java/com/repositorio/cadastrousuario/cadastro/entity/UserEntity.java
@@ -0,0 +1,27 @@
+package com.repositorio.cadastrousuario.cadastro.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "documento", nullable = false)
+    private String document;
+
+    /*
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id", referencedColumnName = "id")
+    private AddressEntity address;
+    */
+
+}
